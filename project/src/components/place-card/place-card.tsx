df02193fa6fa3fe6a58ce7db5549812e09937d92
applebear <applebear@macpaw.com>
2022-02-15 09:44:05 +0200

    feat: add base pages layouts
    add main, login, favorites and property pages, favorite and place cards

---
+++ project/src/components/place-card/place-card.tsx
@@ -0,0 +1,50 @@
+type PlaceCardProps = {
+  price: number;
+  stars: number;
+  name: string;
+  type: string;
+  photo: string;
+  isFav?: boolean;
+  isPremium?: boolean;
+}
+
+function PlaceCard(props: PlaceCardProps): JSX.Element {
+  const { price, stars, name, type, photo, isFav, isPremium } = props;
+
+  return (
+    <article className="cities__place-card place-card">
+      {isPremium && <div className="place-card__mark"><span>Premium</span></div>}
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={photo} width="260" height="200" alt="Place image" />
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className={`place-card__bookmark-button button ${isFav && 'place-card__bookmark-button--active'}`} type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{ width: `${stars * 20}%` }}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{name}</a>
+        </h2>
+        <p className="place-card__type">{type}</p>
+      </div>
+    </article>
+  );
+}
+
+export default PlaceCard;
